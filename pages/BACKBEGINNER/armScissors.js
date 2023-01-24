3db6aa3074321fcc411509b650a0c42c7ddf7c98
Your Name <absida8@gmail.com>
2023-01-25 00:56:22 +0100

    Cat Cow Pose beginner fixed

--- pages/BACKBEGINNER/armScissors.js
+++ pages/BACKBEGINNER/armScissors.js
@@ -28,13 +28,16 @@ export default function ArmScissors() {
       <div className={styles.abdy}>
         <div className={styles.bbdy}>
           <div className={styles.absb}>
-            <Image
-              alt="guy"
-              src={"/back.webp"}
-              height={150}
-              width={300}
-              className={styles.im}
-            />
+            <div className={styles.phoneView}>
+              <Image
+                style={{ borderRadius: "20px" }}
+                alt="guy"
+                src={"/back.webp"}
+                height={180}
+                width={320}
+                className={styles.im}
+              />
+            </div>
             <div className={styles.che1}>BACK BEGINNER</div>
 
             <Link href={"/"}>
@@ -52,12 +55,14 @@ export default function ArmScissors() {
           </div>
           <hr className={styles.vr} />
 
-          <Image
-            alt="guy with the greates image ever"
-            src={"/armScissors.jpeg"}
-            height={200}
-            width={200}
-          />
+          <div className={styles.animation}>
+            <Image
+              alt="guy with the greates image ever"
+              src={"/armScissors.jpeg"}
+              height={200}
+              width={200}
+            />
+          </div>
           <div className={styles.jumtim}>00:30</div>
 
           <div className="stopwatch">
@@ -88,7 +93,7 @@ export default function ArmScissors() {
               <MdArrowForwardIos />
             </Link>
           </div>
-          <h5>8 out of 12</h5>
+          <h5 className={styles.doneOutOfRemaining}>8 out of 12</h5>
         </div>
       </div>
     </div>
