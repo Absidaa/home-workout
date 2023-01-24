1a722ec9f321a6ea30d0f2e17e02b502025de182
Your Name <absida8@gmail.com>
2023-01-25 00:37:41 +0100

    Knee PushUps beginner fixed

--- pages/BACKBEGINNER/kneePushUp.js
+++ pages/BACKBEGINNER/kneePushUp.js
@@ -28,13 +28,16 @@ export default function KneePushUp() {
       <div className={styles.abdy}>
         <div className={styles.bbdy}>
           <div className={styles.absb}>
-            <Image
-              alt="guy"
-              src={"/back.webp"}
-              height={150}
-              width={300}
-              className={styles.im}
-            />
+            <div className={styles.phoneView}>
+              <Image
+                style={{ borderRadius: "20px" }}
+                alt="guy"
+                src={"/back.webp"}
+                height={180}
+                width={320}
+                className={styles.im}
+              />
+            </div>
             <div className={styles.che1}>BACK BEGINNER</div>
 
             <Link href={"/"}>
@@ -52,12 +55,14 @@ export default function KneePushUp() {
           </div>
           <hr className={styles.vr} />
 
-          <Image
-            alt="guy with the greates image ever"
-            src={"/knepush.jpg"}
-            height={200}
-            width={200}
-          />
+          <div className={styles.animation}>
+            <Image
+              alt="guy with the greates image ever"
+              src={"/knepush.jpg"}
+              height={200}
+              width={200}
+            />
+          </div>
           <div className={styles.jumtim}>x14</div>
 
           <div className={styles.backWard}>
@@ -70,7 +75,7 @@ export default function KneePushUp() {
               <MdArrowForwardIos />
             </Link>
           </div>
-          <h5>5 out of 12</h5>
+          <h5 className={styles.doneOutOfRemaining}>5 out of 12</h5>
         </div>
       </div>
     </div>
