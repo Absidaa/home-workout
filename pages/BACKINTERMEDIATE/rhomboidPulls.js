42edfee70455a0c5be2a81ec398ebe23b7b44590
Your Name <absida8@gmail.com>
2023-01-26 15:58:44 +0100

    rhoboid pulls and incline Push Ups done

--- pages/BACKINTERMEDIATE/rhomboidPulls.js
+++ pages/BACKINTERMEDIATE/rhomboidPulls.js
@@ -28,13 +28,16 @@ export default function RhomboidPull() {
       <div className={styles.abdy}>
         <div className={styles.bbdy}>
           <div className={styles.absb}>
-            <Image
-              alt="guy"
-              src={"/backi.jpeg"}
-              height={150}
-              width={300}
-              className={styles.im11}
-            />
+            <div className={styles.phoneView}>
+              <Image
+                style={{ borderRadius: "20px" }}
+                alt="guy"
+                src={"/backi.jpeg"}
+                height={170}
+                width={320}
+                className={styles.im7}
+              />
+            </div>
             <div className={styles.abs3}>BACK INTERMEDIATE</div>
 
             <Link href={"/"}>
@@ -52,12 +55,15 @@ export default function RhomboidPull() {
           </div>
           <hr className={styles.vr} />
 
-          <Image
-            alt="guy with the greates image ever"
-            src={"/roboidPullGif.gif"}
-            height={200}
-            width={200}
-          />
+          <div className={styles.animation}>
+            {" "}
+            <Image
+              alt="guy with the greates image ever"
+              src={"/roboidPullGif.gif"}
+              height={200}
+              width={200}
+            />
+          </div>
           <div className={styles.jumtim}>x12</div>
 
           <div className={styles.backWard}>
@@ -70,7 +76,7 @@ export default function RhomboidPull() {
               <MdArrowForwardIos />
             </Link>
           </div>
-          <h5>4 out of 12</h5>
+          <h5 className={styles.doneOutOfRemaining}>4 out of 12</h5>
         </div>
       </div>
     </div>
