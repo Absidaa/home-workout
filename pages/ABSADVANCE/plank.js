524accde2901f748e97edff081f4584356711da0
Your Name <absida8@gmail.com>
2023-01-26 18:53:10 +0100

    Cobra Stretch and Plank Advancd done

--- pages/ABSADVANCE/plank.js
+++ pages/ABSADVANCE/plank.js
@@ -28,14 +28,16 @@ export default function Plank() {
       <div className={styles.abdy}>
         <div className={styles.bbdy}>
           <div className={styles.absb}>
-            <Image
-              style={{ borderRadius: "20px" }}
-              alt="guy"
-              src={"/absad.jpeg"}
-              height={160}
-              width={300}
-              className={styles.im11}
-            />
+            <div className={styles.phoneView}>
+              <Image
+                style={{ borderRadius: "20px" }}
+                alt="guy"
+                src={"/absad.jpeg"}
+                height={170}
+                width={320}
+                className={styles.im11}
+              />
+            </div>
             <div className={styles.abs7}>ABS ADVANCED</div>
 
             <Link href={"/"}>
@@ -53,12 +55,14 @@ export default function Plank() {
           </div>
           <hr className={styles.vr} />
 
-          <Image
-            alt="guy with the greates image ever"
-            src={"/plankGif.gif"}
-            height={200}
-            width={200}
-          />
+          <div className={styles.animation}>
+            <Image
+              alt="guy with the greates image ever"
+              src={"/plankGif.gif"}
+              height={200}
+              width={200}
+            />
+          </div>
           <div className={styles.jumtim}>1:00</div>
           <div className="stopwatch">
             <div className={styles.numbers}>
@@ -88,7 +92,7 @@ export default function Plank() {
               <MdArrowForwardIos />
             </Link>
           </div>
-          <h5>16 out of 19</h5>
+          <h5 className={styles.doneOutOfRemaining}>16 out of 19</h5>
         </div>
       </div>
     </div>
