25f73334ba6016f4fe2e2fd85aeeccd8cf230003
Your Name <absida8@gmail.com>
2023-01-27 15:18:14 +0100

    Chest Stretch, Cobra Stretch and Declined PushUp advanced done

--- pages/CHESTADVANCE/chestStretch.js
+++ pages/CHESTADVANCE/chestStretch.js
@@ -28,14 +28,16 @@ export default function Cheststretch() {
       <div className={styles.abdy}>
         <div className={styles.bbdy}>
           <div className={styles.absb}>
-            <Image
-              style={{ borderRadius: "20px" }}
-              alt="guy"
-              src={"/chead.jpeg"}
-              height={170}
-              width={300}
-              className={styles.im11}
-            />
+            <div className={styles.phoneView}>
+              <Image
+                style={{ borderRadius: "20px" }}
+                alt="guy"
+                src={"/chead.jpeg"}
+                height={170}
+                width={320}
+                className={styles.im11}
+              />
+            </div>
             <div className={styles.abs8}>CHEST ADVANCED</div>
 
             <Link href={"/"}>
@@ -53,12 +55,15 @@ export default function Cheststretch() {
           </div>
           <hr className={styles.vr} />
 
-          <Image
-            alt="guy with the greates image ever"
-            src={"/chestreh.jpeg"}
-            height={200}
-            width={200}
-          />
+          <div className={styles.animation}>
+            {" "}
+            <Image
+              alt="guy with the greates image ever"
+              src={"/chestreh.jpeg"}
+              height={200}
+              width={200}
+            />
+          </div>
           <div className={styles.jumtim}>00:30</div>
 
           <div className="stopwatch">
@@ -90,7 +95,7 @@ export default function Cheststretch() {
               <MdArrowForwardIos />
             </Link>
           </div>
-          <h5>13 out of 13</h5>
+          <h5 className={styles.doneOutOfRemaining}>13 out of 13</h5>
         </div>
       </div>
     </div>
