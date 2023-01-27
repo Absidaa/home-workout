dcc6b2f65df3f5129d19c1810e6ef017dcbbfad1
Your Name <absida8@gmail.com>
2023-01-27 16:18:11 +0100

    Tricep Stretch left and Right arm advanced done

--- pages/ARMADVANCE/tricepStretchRight.js
+++ pages/ARMADVANCE/tricepStretchRight.js
@@ -28,14 +28,16 @@ export default function TricepStretchRight() {
       <div className={styles.abdy}>
         <div className={styles.bbdy}>
           <div className={styles.absb}>
-            <Image
-              style={{ borderRadius: "20px" }}
-              alt="guy"
-              src={"/armad.jpeg"}
-              height={160}
-              width={300}
-              className={styles.im11}
-            />
+            <div className={styles.phoneView}>
+              <Image
+                style={{ borderRadius: "20px" }}
+                alt="guy"
+                src={"/armad.jpeg"}
+                height={170}
+                width={320}
+                className={styles.im11}
+              />
+            </div>
             <div className={styles.abs7}>ARM ADVANCED</div>
 
             <Link href={"/"}>
@@ -53,12 +55,14 @@ export default function TricepStretchRight() {
           </div>
           <hr className={styles.vr} />
 
-          <Image
-            alt="guy with the greates image ever"
-            src={"/TricepStretchLeftGif.jpg"}
-            height={200}
-            width={200}
-          />
+          <div className={styles.animation}>
+            <Image
+              alt="guy with the greates image ever"
+              src={"/TricepStretchLeftGif.jpg"}
+              height={200}
+              width={200}
+            />
+          </div>
           <div className={styles.jumtim}>00:30</div>
           <div className="stopwatch">
             <div className={styles.numbers}>
@@ -89,7 +93,7 @@ export default function TricepStretchRight() {
               <MdArrowForwardIos />
             </Link>
           </div>
-          <h5>16 out of 18</h5>
+          <h5 className={styles.doneOutOfRemaining}>16 out of 18</h5>
         </div>
       </div>
     </div>
